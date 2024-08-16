73884f4605e96841a6cb57a332d53bde205daed9
James Begin <103123677+James-Begin@users.noreply.github.com>
2024-08-16 16:23:03 -0400

    Update BasicFormulas.py

--- BasicFormulas.py
+++ BasicFormulas.py
@@ -7,6 +7,7 @@ from scipy.stats import norm
 def blackscholes(price, strike, exp, rfr, vol):
     #first intermediate step
     #
+    exp = exp/365
     d1 = ((np.log(price / strike)) + (exp * (rfr + ((vol**2)/2)))) / (vol * (exp**0.5))
 
     #second step
@@ -18,36 +19,37 @@ def blackscholes(price, strike, exp, rfr, vol):
 def montecarlo(price, strike, exp, rfr, vol):
     #first simulate prices
     #init price movements over time (1000 simulations can be changed)
+    timeperstep = (exp**2) / 365
     numsim = 1000
     simulate = np.zeros((exp, numsim))
     simulate[0] = price
 
     for time in range(1, exp):
         #random values
-        rand = np.random.normal(numsim)
+        rand = np.random.standard_normal(numsim)
         #fill in prices at each time
         simulate[time] = simulate[time-1] * np.exp((rfr - ((vol**2)/2)) *
-                        (exp/numsim) + (vol * ((exp/numsim)**0.5) * rand))
+                        (timeperstep) + (vol * ((timeperstep)**0.5) * rand))
 
-    return [np.exp(-rfr * exp) * (1 / numsim) * np.sum(max(simulate[-1] - strike, 0)),
-            np.exp(-rfr * exp) * (1 / numsim) * np.sum(max(strike - simulate[-1], 0))]
+    return [np.exp(-rfr * exp/365) * (1 / numsim) * np.sum(np.maximum(simulate[-1] - strike, 0)),
+            np.exp(-rfr * exp/365) * (1 / numsim) * np.sum(np.maximum(strike - simulate[-1], 0))]
 
 def binomial(price, strike, exp, rfr, vol):
-    upfactor = np.exp(vol)
+    timeperstep = (exp**2) / 365
+    upfactor = np.exp(vol * timeperstep**0.5)
     downfactor = 1/upfactor
-
     Vcall = np.zeros(exp+1)
     Vput = np.zeros(exp+1)
-    prices = np.array([(price * (upfactor**i) * (downfactor**(exp-i)) for i in range(exp+1))])
-    compound = np.exp(rfr)
+    prices = np.array([((price * upfactor**i * downfactor**(exp-i)) for i in range(exp+1))])
+    compound = np.exp(rfr * timeperstep)
     upprob = (compound-downfactor) / (upfactor - downfactor)
     downprob = 1.0-upprob
 
-    Vcall[:] = np.maximum(prices - strike, 0)
-    Vput[:] = np.maximum(strike - prices, 0)
+    Vcall[:] = np.maximum(prices - strike, 0.0)
+    Vput[:] = np.maximum(strike - prices, 0.0)
     for i in range(exp-1, -1, -1):
-        Vcall[:-1] = np.exp(-rfr) * (upprob * Vcall[1:] + downprob * Vcall[:-1])
-        Vput[:-1] = np.exp(-rfr) * (upprob * Vput[1:] + downprob * Vput[:-1])
+        Vcall[:-1] = np.exp(-rfr * timeperstep) * (upprob * Vcall[1:] + downprob * Vcall[:-1])
+        Vput[:-1] = np.exp(-rfr * timeperstep) * (upprob * Vput[1:] + downprob * Vput[:-1])
 
     return [Vcall[0], Vput[0]]
 
