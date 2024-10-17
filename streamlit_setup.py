1be43f07b2f920c0a7212dbfdb9600e633ffab81
James Begin <j3begin@uwaterloo.ca>
2024-10-16 23:00:35 -0400

    update streamlit

--- streamlit_setup.py
+++ streamlit_setup.py
@@ -227,8 +227,8 @@ def plot_heatmap(bs_model, spot_range, exp_range, strike, pp):
                 interest_rate=bs_model.interest_rate
             )
             bs_temp.calculate_prices()
-            call_prices[i, j] = bs_temp.call - pp
-            put_prices[i, j] = bs_temp.put - pp
+            call_prices[i, j] = 100 * ((bs_temp.call - pp) / pp)
+            put_prices[i, j] = 100 * ((bs_temp.put - pp) / pp)
 
     # Plotting Call Price Heatmap
     fig_call, ax_call = plt.subplots(figsize=(10, 8))
