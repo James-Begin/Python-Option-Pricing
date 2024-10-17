b395a2920a16bad21efe3f8813565975da6a4011
James Begin <j3begin@uwaterloo.ca>
2024-10-17 19:01:44 -0400

    update streamlit

--- streamlit_setup.py
+++ streamlit_setup.py
@@ -27,7 +27,7 @@ st.markdown("""
 /* Adjust the size and alignment of the CALL and PUT value containers */
 .metric-container {
     display: flex;
-    justify-content: center;
+    justify-content: left
     align-items: center;
     padding: 8px; /* Adjust the padding to control height */
     width: auto; /* Auto width for responsiveness, or set a fixed width if necessary */
@@ -38,7 +38,7 @@ st.markdown("""
 .metric-call {
     background-color: #1ba122; /* Light green background */
     color: "#3d3c35";
-    margin-right: 6px; /* Spacing between CALL and PUT */
+    margin-right: 10px; /* Spacing between CALL and PUT */
     border-radius: 10px; /* Rounded corners */
 }
 
@@ -57,7 +57,7 @@ st.markdown("""
 
 /* Style for the label text */
 .metric-label {
-    font-size: 1.5rem; /* Adjust font size */
+    font-size: 1rem; /* Adjust font size */
     margin-bottom: 2px; /* Spacing between label and value */
 }
 
